18121bd21c653d5b901a3a772955f6ab32ef16bb
AlexxSap <alexxandrsap@mail.ru>
2024-10-21 21:18:42 +0300

    add ivy

--- init.el
+++ init.el
@@ -25,8 +25,8 @@
        ;;(corfu +orderless)  ; complete with cap(f), cape and a flying feather!
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ;; (ivy -childframe +icons +fuzzy)               ; a search engine for love and life
-       vertico           ; the search engine of the future
+       (ivy -childframe +icons +fuzzy)               ; a search engine for love and life
+       ;; vertico           ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
