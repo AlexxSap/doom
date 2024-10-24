8f2f42fe47bcb63a2e22cd0e5a0162ca8f5d1c9e
AlexxSap <alexxandrsap@mail.ru>
2024-10-24 15:24:12 +0300

    edit custom.el

--- custom.el
+++ custom.el
@@ -5,13 +5,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages '(markdown-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-block-begin-line ((t (:height 0.8 :extend t :weight bold))))
- '(org-block-end-line ((t (:height 0.8 :extend t :weight bold)))))
 
 (defvar nb/current-line '(0 . 0)
   "(start . end) of current line in current buffer")
