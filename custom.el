7c949188b49b12ae6e71b22d99f08caa1793e725
AlexxSap <alexxandrsap@mail.ru>
2025-05-26 15:31:09 +0300

    add dirvish to packages

--- custom.el
+++ custom.el
@@ -5,7 +5,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(haskell-ts-mode haskell-tab-indent flymake-haskell-multi lsp-haskell markdown-mode)))
+   '(dirvish flymake-haskell-multi haskell-tab-indent haskell-ts-mode lsp-haskell
+     markdown-mode)))
 
 (defvar nb/current-line '(0 . 0)
   "(start . end) of current line in current buffer")
