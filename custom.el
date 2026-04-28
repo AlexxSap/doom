6cc8aff9c8c8aa65669ecaabead0ae5c370160a3
AlexxSap <alexxandrsap@mail.ru>
2026-04-28 21:47:11 +0300

    add flycheck to emacs config

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(company company-box company-org-block flymake-haskell-multi haskell-tab-indent
+   '(company company-box company-org-block flymake-haskell-multi haskell-tab-indent flycheck-haskell
      haskell-ts-mode lsp-haskell markdown-mode org-present org-tree-slide
      pdf-tools protobuf-mode visual-fill-column)))
 
